87cefbb53693253347bd6cffe2ccf6c60a943d83
Tao <unitedstatesofsocilistrepublic@gmail.com>
2022-05-16 08:54:24 +0700

    upcode

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <div></div>
+    <table>
+      <td>C
+        <tr>A
+        </tr>
+        <tr>B
+        </tr>
+      </td>
+    </table>
+  </main>
+</template>
+
+<script>
+export default {
+   
+}
+</script>
+
+<style>
+
+</style>
